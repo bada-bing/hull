4a01be22bc4e820bc5b02440eb50f40a3855aead
Miki <bada.bing.bing@live.com>
2024-05-12 20:58:23 +0200

    ✨ [examples] Add initial todos components

---
+++ examples/todos.ts
@@ -0,0 +1,26 @@
+// Generate App Component and mount it to body
+
+import { VText, h } from "../src/runtime/h";
+import { mountDOM } from "../src/runtime/mount-dom";
+
+
+/**
+ * section
+ * h1 My Blog
+ * Welcome to My Blog
+ */
+
+const output = h('section', {}, [
+    h('h1', {}, ['My Blog']),
+    h('p', {}, ['Welcome to My Blog!'])
+]);
+
+
+const textExample: VText = { type: "text", value: "an examplary text to be shown on the screen" }
+
+let bodyElement = document.body as HTMLBodyElement;
+const div = document.createElement('div');
+div.textContent = "what' up, little dog"
+bodyElement.appendChild(div);
+mountDOM(textExample, bodyElement)
+mountDOM(output, bodyElement)
