e1de206809f5ce4711a619cdbd637f53845e43d2
Miki <bada.bing.bing@live.com>
2024-05-22 23:11:58 +0200

    🚨 [Prettier] Formatting

--- src/runtime/h.test.ts
+++ src/runtime/h.test.ts
@@ -1,12 +1,12 @@
-import { describe, test, expect, assert } from "vitest"
-import { type VElement, type VText, h } from "./h"
-import { _notNull } from "./arrays"
+import { describe, test, expect, assert } from "vitest";
+import { type VElement, type VText, h } from "./h";
+import { _notNull } from "./arrays";
 
 /**
  * Virtual Element which resembles the following HTML structure (i.e., form DOM element with its children).
- * 
+ *
  * h() function should ensure that such VElement is created.
- * 
+ *
  * @example
  * <form class="login-form" action="login">
  *   <input type="text" name="user">
@@ -15,45 +15,78 @@ import { _notNull } from "./arrays"
  * </form>
  */
 const expectedRes: VElement = {
-    type: "element",
-    tag: "form",
-    props: { class: "login-form", action: "login" },
-    children: [
-        { type: "element", tag: "input", props: { type: "text", name: "user" }, children: [] },
-        { type: "element", tag: "input", props: { type: "text", name: "pass" }, children: [] },
-        { type: "element", tag: "button", props: { on: { click: "login" } }, children: [{ type: "text", value: "Log in" }] }]
-}
+  type: "element",
+  tag: "form",
+  props: { class: "login-form", action: "login" },
+  children: [
+    {
+      type: "element",
+      tag: "input",
+      props: { type: "text", name: "user" },
+      children: [],
+    },
+    {
+      type: "element",
+      tag: "input",
+      props: { type: "text", name: "pass" },
+      children: [],
+    },
+    {
+      type: "element",
+      tag: "button",
+      props: { on: { click: "login" } },
+      children: [{ type: "text", value: "Log in" }],
+    },
+  ],
+};
 
 describe("h", () => {
-    let inputUser: VElement;
-    let inputPass: VElement;
-    let buttonLogIn: VElement;
+  let inputUser: VElement;
+  let inputPass: VElement;
+  let buttonLogIn: VElement;
 
-    test("should create form VElement with its children and props", () => {
-        inputUser = createVElement('input', { type: "text", name: "user" })
-        inputPass = createVElement('input', { type: "text", name: "pass" })
-        buttonLogIn = createVElement('button', { on: { click: "login" } }, ['Log in'])
+  test("should create form VElement with its children and props", () => {
+    inputUser = createVElement("input", { type: "text", name: "user" });
+    inputPass = createVElement("input", { type: "text", name: "pass" });
+    buttonLogIn = createVElement("button", { on: { click: "login" } }, [
+      "Log in",
+    ]);
 
-        const formEl = createVElement('form', { class: "login-form", action: "login" }, [inputUser, inputPass, null, buttonLogIn])
+    const formEl = createVElement(
+      "form",
+      { class: "login-form", action: "login" },
+      [inputUser, inputPass, null, buttonLogIn],
+    );
 
-        expect(formEl).toEqual(expectedRes);
-    })
+    expect(formEl).toEqual(expectedRes);
+  });
 
-    function createVElement(tag: string, props: {}, children?: (string | VElement | null)[]) {
-        const el = h(tag, props, children)
+  function createVElement(
+    tag: string,
+    props: {},
+    children?: (string | VElement | null)[],
+  ) {
+    const el = h(tag, props, children);
 
-        let expChildren: (VElement | VText)[] = []
+    let expChildren: (VElement | VText)[] = [];
 
-        if (children) {
-            expChildren = children.filter(_notNull).map(child => {
-                return (typeof child === "string") ? { type: "text", value: child } as const : child
-            })
-        }
-
-        const expected = { type: "element", tag, props, children: expChildren } as const
-
-        assert.deepEqual(el, expected, `Child of type ${tag}`)
-
-        return el;
+    if (children) {
+      expChildren = children.filter(_notNull).map((child) => {
+        return typeof child === "string"
+          ? ({ type: "text", value: child } as const)
+          : child;
+      });
     }
-})
+
+    const expected = {
+      type: "element",
+      tag,
+      props,
+      children: expChildren,
+    } as const;
+
+    assert.deepEqual(el, expected, `Child of type ${tag}`);
+
+    return el;
+  }
+});
